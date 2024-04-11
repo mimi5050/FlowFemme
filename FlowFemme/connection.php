e3a28b55b688c376a70a129a22e895f1e0459356
mimi5050 <miriam.uwingabiye@ashesi.edu.gh>
2024-04-11 16:14:56 +0000

    sdifdffssg

--- FlowFemme/connection.php
+++ FlowFemme/connection.php
@@ -13,7 +13,7 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully!";
+echo " ";
 
 ?>
 
