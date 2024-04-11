b5b9048ab3a65f7510094b1bc4f2caeada33c6f2
mimi5050 <miriam.uwingabiye@ashesi.edu.gh>
2024-04-11 15:45:39 +0000

    update

--- FlowFemme/connection.php
+++ FlowFemme/connection.php
@@ -1,18 +1,20 @@
 <?php
 // Database connection parameters
-$servername = "localhost";
-$username = "root";
-$dbname = "FlowFemme";
-
+$servername = "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "nv8ts7qnvuhleqhb";
+$password = "sf4fgiofbs3n5mol";
+$dbname = "qu8h316ipkc2b6nx";
+$port = 3306;
 
 // Create connection
-$conn = new mysqli($servername, $username, "", $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo " "; 
+echo "Connected successfully!";
+
 ?>
 
 
