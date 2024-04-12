ad196adf4822e74b52bc3477689a18019d5f88a5
mimi5050 <miriam.uwingabiye@ashesi.edu.gh>
2024-04-12 15:54:17 +0000

    sdddjkguuyxuue

--- FlowFemme/period_predictions.php
+++ FlowFemme/period_predictions.php
@@ -355,13 +355,13 @@ if ($result) {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        background-color: #fff;
         border: 1px solid #ccc;
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         padding: 20px;
         max-width: 300px;
         text-align: center;
         z-index: 1000;
+        background-color: rgba(0, 0, 0, 0.5);
     }
 
     .popup div {
